32e210074113f40a3b4a0ed472c17af8313f74b9
Caio <c.santosggwp@gmail.com>
2023-08-22 19:47:05 -0300

    add title and urlinput

--- YoutubeDownload.py
+++ YoutubeDownload.py
@@ -8,6 +8,16 @@ window.geometry('350x380')
 window.config(bg='#15171a')
 window.resizable(width=False,height=False)
 
+# LABEL-YOUTUBE 
+labelYoutube = Label(window,width=20,height=1,text='YoutubeDownloader',bg='#a80f0f',font='Arial 18',fg='white')
+labelYoutube.place(x=25,y=10)
 
+# LABEL-URL
+labelURL = Label(window,width=5,height=1,text='URL:',bg='#15171a',fg='white',font=('Aial 10 '))
+labelURL.place(x=50,y=70)
+
+# URL-INPUT
+inputURL = Entry(window,width=30,relief=GROOVE)
+inputURL.place(x=90,y=73)
 
 window.mainloop()
