acdacbad0b466dba424ac72dcb5153bedce026fc
Caio <c.santosggwp@gmail.com>
2023-08-22 19:43:41 -0300

    config window

---
+++ YoutubeDownload.py
@@ -0,0 +1,13 @@
+from tkinter.ttk import *
+from tkinter import *
+from pytube import YouTube
+
+window = Tk()
+window.title('YoutubeDownload')
+window.geometry('350x380')
+window.config(bg='#15171a')
+window.resizable(width=False,height=False)
+
+
+
+window.mainloop()
